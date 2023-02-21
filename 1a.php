f1a0142dba1f5101f31ddb6a9db824a5162f7b8b
DickyDD6 <125367943+DickyDD6@users.noreply.github.com>
2023-02-21 08:33:17 +0700

    Add files via upload

---
+++ 1a.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Tugas 1a</title>
+</head>
+<body>
+    <?php 
+    $benda = "Topi";
+    $bentuk = "Bundar";
+    ?>
+    <h2>
+    <?php
+       echo "<i> \"$benda Saya $bentuk , $bentuk $benda Saya\"";
+       
+    ?>
+
+
+    </h2>
+</body>
+</html>
